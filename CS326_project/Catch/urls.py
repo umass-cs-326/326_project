58366ea944328c1c01ac62a32076f7a8eadd19c0
jhui04 <jhui@umass.edu>
2018-10-02 13:15:39 -0400

    added urls.py for Catch app with redirects

--- CS326_project/Catch/urls.py
+++ CS326_project/Catch/urls.py
@@ -4,6 +4,10 @@ from Catch import views
 
 urlpatterns = [
     url('homePage', views.home),
+    url('eventsPage', views.events),
+    url('mapPage', views.map),
+    url('profilePage', views.profile),
     url('aboutPage', views.about),
-    url('')
+    url('navbar', views.navbar),
+
 ]
--- CS326_project/Catch/views.py
+++ CS326_project/Catch/views.py
@@ -4,5 +4,17 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'Catch/homePage.html',)
 
+def events(request):
+    return render(request, 'Catch/eventsPage.html')
+
+def map(request):
+    return render(request, 'Catch/mapPage.html',)
+
+def profile(request):
+    return render(request, 'Catch/profilePage.html')
+
 def about(request):
     return render(request, 'Catch/aboutPage.html')
+
+def navbar(request):
+    return render(request, 'Catch/events.html')
