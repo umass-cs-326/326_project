dd5874708a3ee08ba49fb4368939746ae8872d18
jhui04 <jhui@umass.edu>
2018-10-07 20:22:10 -0400

    Added simple event model and url mapping for navbar

--- CS326_project/CS326_project/urls.py
+++ CS326_project/CS326_project/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('Catch', include('Catch.urls')),
+    url('Catch/', include('Catch.urls')),
 ]
--- CS326_project/Catch/urls.py
+++ CS326_project/Catch/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 from Catch import views
 
-urlpatterns = [
-    url('homePage', views.home),
-    url('eventsPage', views.events),
-    url('mapPage', views.map),
-    url('profilePage', views.profile),
-    url('aboutPage', views.about),
-    url('navbar', views.navbar),
 
+urlpatterns = [
+    url('homePage', views.home, name = 'homePage'),
+    url('eventsPage', views.events, name = 'eventsPage'),
+    url('profilePage', views.profile, name = 'profilePage'),
+    url('mapPage', views.map, name = 'mapPage'),
+    url('aboutPage', views.about, name = 'aboutPage'),
+    # url('navbar', views.navbar),
 ]
