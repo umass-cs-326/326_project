e52f6f099d815bed8b8f319a141b0750efe5973b
Aristotel <afani1997@gmail.com>
2018-10-17 15:23:52 -0400

    Updated Django Settings.

--- src/aTlas/aTlas/settings.py
+++ src/aTlas/aTlas/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'aTlas.apps.UmarketConfig'
+    'umarket.apps.UmarketConfig'
 ]
 
 MIDDLEWARE = [
--- src/aTlas/aTlas/urls.py
+++ src/aTlas/aTlas/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('umarket/', include('umarket.urls')),
+    path('', RedirectView.as_view(url='/umarket/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
