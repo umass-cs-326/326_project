53a9e8f6f209a633f5649fbe42f84ac5f85a2eeb
Matthew Dahl <mcdahl@umass.edu>
2018-10-18 17:58:29 +0000

    adding models initial

--- src/A1Sauss/classdoor/admin.py
+++ src/A1Sauss/classdoor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from classdoor.models import ClassDesc
 
 # Register your models here.
+
+@admin.register(ClassDesc)
+class ClassDescAdmin(admin.ModelAdmin):
+    list_display = ("name", "teacher", "description", "rating")
+    fields = ["name", "teacher", "description", "rating"]
--- src/A1Sauss/classdoor/models.py
+++ src/A1Sauss/classdoor/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class ClassDesc(models.Model):
+   """Model representing an library event."""
+
+   name = models.CharField(max_length=200)
+   #tags = models.models.ManyToManyField(Class, help_text="Select a preReq for this class")
+   teacher = models.CharField(max_length=200)
+   #reviews = models.ArrayField()
+   description = models.CharField(max_length=200)
+   rating = models.IntegerField()
+   avgGrade = models.IntegerField(max_length=10)
+   #preReqs = models.ManyToManyField(Class, help_text="Select a preReq for this class")
+   #subject = models.ManyToManyField(Subject, help_text="Select a preReq for this class")
+   #university = models.CharField(max_length=200)
+   #genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+
+
+   class Meta:
+       ordering = ['name']
+
+   def get_absolute_url(self):
+       """Returns the url to access a particular author instance."""
+       return reverse('event-detail', args=[str(self.id)])
+
+   def __str__(self):
+       """String for representing the Model object."""
+       return f'{self.title}'
