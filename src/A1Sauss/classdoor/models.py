4fa56c3272b2c7010ba5e3f896e220e39e702549
Matthew Dahl <mcdahl@umass.edu>
2018-10-19 00:34:53 +0000

    finished rough models

--- src/A1Sauss/classdoor/models.py
+++ src/A1Sauss/classdoor/models.py
@@ -8,14 +8,13 @@ class ClassDesc(models.Model):
    name = models.CharField(max_length=200)
    #tags = models.models.ManyToManyField(Class, help_text="Select a preReq for this class")
    teacher = models.CharField(max_length=200)
-   #reviews = models.ArrayField()
+   reviews = models.ManyToManyField('Review', help_text="Reviews for this class")
    description = models.CharField(max_length=200)
-   rating = models.IntegerField()
-   avgGrade = models.IntegerField(max_length=10)
-   #preReqs = models.ManyToManyField(Class, help_text="Select a preReq for this class")
-   #subject = models.ManyToManyField(Subject, help_text="Select a preReq for this class")
-   #university = models.CharField(max_length=200)
-   #genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+   rating = models.DecimalField(default=0.0, max_digits=4, decimal_places=2)
+   avgGrade = models.DecimalField(default=0.0, max_digits=4, decimal_places=2)
+   preReqs = models.ManyToManyField('self', help_text="Select a preReq for this class", blank=True)
+   subject = models.ForeignKey('Subject', help_text="Select a subject for this class",  on_delete=models.SET_NULL, null=True)
+   universityName = models.ForeignKey('University', max_length=200,  on_delete=models.SET_NULL, null=True)
 
 
 
@@ -28,4 +27,96 @@ class ClassDesc(models.Model):
 
    def __str__(self):
        """String for representing the Model object."""
+       return f'{self.name}'
+
+
+class Review(models.Model):
+   """Model representing an library event."""
+
+   title = models.CharField(max_length=200)
+   text = models.CharField(max_length=500)
+   starRating = models.DecimalField(max_digits=3, decimal_places=2)
+   gradeReceived = models.DecimalField(max_digits=3, decimal_places=2)
+   #tags = models.IntegerField(default=1)
+   classDesc = models.ForeignKey('ClassDesc', help_text="Select a class for this description",  on_delete=models.SET_NULL, null=True)
+   author = models.ForeignKey('User', help_text="Select a user for this review",  on_delete=models.SET_NULL, null=True)
+   likes = models.IntegerField(default=0)
+   dislikes = models.IntegerField(default=0)
+
+
+
+   class Meta:
+       ordering = ['title']
+
+   def get_absolute_url(self):
+       """Returns the url to access a particular author instance."""
+       return reverse('event-detail', args=[str(self.id)])
+
+   def __str__(self):
+       """String for representing the Model object."""
        return f'{self.title}'
+
+
+class University(models.Model):
+   """Model representing an library event."""
+
+   name = models.CharField(max_length=200)
+   location = models.CharField(max_length=500)
+   classes = models.ManyToManyField('ClassDesc')
+   subjectsOffered = models.ManyToManyField('Subject', blank=True)
+   
+
+   class Meta:
+       ordering = ['name']
+
+   def get_absolute_url(self):
+       """Returns the url to access a particular author instance."""
+       return reverse('event-detail', args=[str(self.id)])
+
+   def __str__(self):
+       """String for representing the Model object."""
+       return f'{self.name}'
+
+
+class User(models.Model):
+   """Model representing an library event."""
+
+   username = models.CharField(max_length=15)
+   email = models.CharField(max_length=30)
+   password = models.CharField(max_length=20)
+   major = models.ManyToManyField('Subject')
+   writtenReviews = models.ManyToManyField('Review', blank=True)
+   
+
+   class Meta:
+       ordering = ['username']
+
+   def get_absolute_url(self):
+       """Returns the url to access a particular author instance."""
+       return reverse('event-detail', args=[str(self.id)])
+
+   def __str__(self):
+       """String for representing the Model object."""
+       return f'{self.username}'
+
+class Subject(models.Model):
+   """Model representing an library event."""
+
+   name = models.CharField(max_length=40)
+   universityName = models.ForeignKey('University',  on_delete=models.SET_NULL, null=True, related_name='+')
+   
+
+   class Meta:
+       ordering = ['name']
+
+   def get_absolute_url(self):
+       """Returns the url to access a particular author instance."""
+       return reverse('event-detail', args=[str(self.id)])
+
+   def __str__(self):
+       """String for representing the Model object."""
+       return f'{self.name}'
+
+
+
+
