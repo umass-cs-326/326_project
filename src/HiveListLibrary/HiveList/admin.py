f14f47e6aa6d6cf0a127925af1c5f0c50676519a
Caleb-Carr <vagrant@vagrant.vm>
2018-10-20 19:54:57 +0000

    Edited admin.py and views.py

--- src/HiveListLibrary/HiveList/admin.py
+++ src/HiveListLibrary/HiveList/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
+from HiveList.models import Playlist, Contributor, Artist, Song, Genre, SongInstance, VoteInstance
+
 # Register your models here.
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    #displays
+
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    #displays
+
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    #displays
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    #displays
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    #displays
+
+
+@admin.register(SongInstance)
+class SongInstanceAdmin(admin.ModelAdmin):
+    #displays
+
+
+@admin.register(VoteInstance)
+class VoteInstanceAdmin(admin.ModelAdmin):
+    #displays
--- src/HiveListLibrary/HiveList/views.py
+++ src/HiveListLibrary/HiveList/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
-from HiveList.models import 
+from HiveList.models import Playlist, Contributor, Artist, Song, Genre, SongInstance, VoteInstance
 
 # Create your views here.
+def index(request):
 
+
+    # Render the HTML tmeplate index.html with the data in the context variable
+    return render(request, "index.html", context=context)
