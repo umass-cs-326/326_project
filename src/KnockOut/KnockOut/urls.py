2d3259bc4360efae5a7397fd92180e1f13addafb
winglungchau <winglungchau@umass.edu>
2018-10-21 14:16:58 -0400

    added url mapping

--- src/KnockOut/KnockOut/urls.py
+++ src/KnockOut/KnockOut/urls.py
@@ -15,7 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    path('', include('Flick.urls')),
+]
+
+# urlpatterns += [
+#     path('', RedirectView.as_view(url='/Flick/')),
+# ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
