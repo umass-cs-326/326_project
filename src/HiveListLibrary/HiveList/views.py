fce01bc08c4a1b252701d995a62291a901e80d7b
Caleb-Carr <vagrant@vagrant.vm>
2018-10-21 17:36:21 +0000

    updated views.py

--- src/HiveListLibrary/HiveList/views.py
+++ src/HiveListLibrary/HiveList/views.py
@@ -8,3 +8,40 @@ def index(request):
 
     # Render the HTML tmeplate index.html with the data in the context variable
     return render(request, "index.html", context=context)
+
+
+def currentPlaylist(request):
+
+
+    # Render the HTML tmeplate index.html with the data in the context variable
+    return render(request, "currentPlaylist.html", context=context)
+
+def Explore(request):
+
+
+    # Render the HTML tmeplate index.html with the data in the context variable
+    return render(request, "Explore.html", context=context)
+
+def Home(request):
+
+
+    # Render the HTML tmeplate index.html with the data in the context variable
+    return render(request, "Home.html", context=context)
+
+def myLists(request):
+
+
+    # Render the HTML tmeplate index.html with the data in the context variable
+    return render(request, "myLists.html", context=context)
+
+def playlistSettings(request):
+
+
+    # Render the HTML tmeplate index.html with the data in the context variable
+    return render(request, "playlistSettings.html", context=context)
+
+def profile(request):
+
+
+    # Render the HTML tmeplate index.html with the data in the context variable
+    return render(request, "profile.html", context=context)
