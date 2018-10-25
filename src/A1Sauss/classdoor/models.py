e0f1e198bfc9b0ea0c2af36f13f86e3be845f44f
zihangzhou <43583714+zihangzhou@users.noreply.github.com>
2018-10-25 14:27:39 -0400

    Add files via upload
    new fix

--- src/A1Sauss/classdoor/models.py
+++ src/A1Sauss/classdoor/models.py
@@ -3,31 +3,26 @@ from django.db import models
 class ClassDesc(models.Model):
    """Model representing a course profile."""
    name = models.CharField(max_length=200)
-   #tags = models.models.ManyToManyField(Class, help_text="Select a preReq for this class")
    teacher = models.CharField(max_length=200)
-   #modified review: one class can have many reviews while a review is for one class
-   reviews = models.ManyToManyField('Review', blank=True, help_text="Review for this class")
-   #added help_text for description
    description = models.CharField(max_length=200, help_text = "Description of the course")
-   starRating = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
-   #changed name from aveGrade to grade
-   averageGrade = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
-   #Changed help_text info
+   rating = models.DecimalField(default=0.0, max_digits=4, decimal_places=2)
+   reviews = models.ForeignKey('Review', max_length = 500, on_delete = models.SET_NULL, null = True, help_text="Review for this class")
+   grade = models.DecimalField(default=0.0, max_digits=4, decimal_places=2)
    preReqs = models.ManyToManyField('self', help_text="PreReqs for this class", blank=True)
-   subject = models.ForeignKey('Subject', help_text="Select a subject for this class",  on_delete=models.SET_NULL, null=True)
-   #Changed universityName to university
-   universityName = models.ForeignKey('University', max_length=200,  on_delete=models.SET_NULL, null=True)
+   subjects = models.ForeignKey('Subject', help_text="Select a subject for this class",  on_delete=models.SET_NULL, null=True)
+   universities = models.ForeignKey('University', max_length=200,  on_delete=models.SET_NULL, null=True)
+   likes = models.IntegerField(default=0)
+   dislikes = models.IntegerField(default=0)
 
    class Meta:
        ordering = ['name']
 
    def get_absolute_url(self):
-       #modified text
        """Returns the url to access a particular class."""
        return reverse('class-detail', args=[str(self.id)])
 
    def __str__(self):
-       """String for representing the Model object."""
+       """String for representing the ClassDesc object."""
        return f'{self.name}'
 
 #-------------------part 1------------------------#
@@ -36,55 +31,49 @@ class Review(models.Model):
    """Model representing a review page."""
    title = models.CharField(max_length=200)
    text = models.CharField(max_length=500)
-   starRating = models.DecimalField(max_digits=10, decimal_places=2)
-   gradeReceived = models.DecimalField(max_digits=10, decimal_places=2)
-   #added date of review
+   starRating = models.DecimalField(max_digits=3, decimal_places=2)
+   gradeReceived = models.DecimalField(max_digits=3, decimal_places=2)
    date = models.DateField(null=True, blank=True)
-   #tags = models.IntegerField(default=1)
-   #foreignkey for classDesc?
-   classDesc = models.ForeignKey('ClassDesc', help_text="Select a class for this description",  on_delete=models.SET_NULL, null=True)
+   tags = models.CharField(max_length = 10, help_text = "Describe course difficulty as easy/medium/hard/fun/boring/netural")
+   #classDesc = models.ForeignKey('ClassDesc', help_text="Select a class for this description",  on_delete=models.SET_NULL, null=True)
    author = models.ForeignKey('User', help_text="Select a user for this review",  on_delete=models.SET_NULL, null=True)
-   likes = models.IntegerField(default=0)
-   dislikes = models.IntegerField(default=0)
-
+ 
    class Meta:
        ordering = ['title']
 
    def get_absolute_url(self):
-       #modified text
        """Returns the url to access a particular review."""
        return reverse('review-detail', args=[str(self.id)])
 
    def __str__(self):
-       """String for representing the Model object."""
+       """String for representing the Review object."""
        return f'{self.title}'
 
 #-------------------part 2------------------------#
  
 class University(models.Model):
-   """Model representing a University."""
+   """Model representing an library event."""
 
    name = models.CharField(max_length=200)
    location = models.CharField(max_length=500)
    classes = models.ManyToManyField('ClassDesc')
-   subjects = models.ManyToManyField('Subject', blank=True)
+   subjectsOffered = models.ManyToManyField('Subject', blank=True)   
 
    class Meta:
        ordering = ['name']
 
    def get_absolute_url(self):
-       #modified text
        """Returns the url to access a particular university."""
        return reverse('university-detail', args=[str(self.id)])
 
    def __str__(self):
-       """String for representing the Model object."""
+       """String for representing the University object."""
        return f'{self.name}'
 
 #-------------------part 3------------------------#
  
 class User(models.Model):
-   """Model representing a User."""
+   """Model representing an library event."""
 
    username = models.CharField(max_length=15)
    email = models.CharField(max_length=30)
@@ -96,33 +85,30 @@ class User(models.Model):
        ordering = ['username']
 
    def get_absolute_url(self):
-       #modified text
        """Returns the url to access a particular author instance."""
        return reverse('user-detail', args=[str(self.id)])
 
    def __str__(self):
-       """String for representing the Model object."""
+       """String for representing the User object."""
        return f'{self.username}'
 
 #-------------------part 4------------------------#
  
 class Subject(models.Model):
-   """Model representing a Subject."""
+   """Model representing an library event."""
 
    name = models.CharField(max_length=40)
-   abbreviation = models.CharField(max_length=4, default="SUBJ")
    universityName = models.ForeignKey('University',  on_delete=models.SET_NULL, null=True, related_name='+')
-
+   
    class Meta:
        ordering = ['name']
 
    def get_absolute_url(self):
-       #modified text
        """Returns the url to access a particular subject."""
        return reverse('subject-detail', args=[str(self.id)])
 
    def __str__(self):
-       """String for representing the Model object."""
+       """String for representing the Subject object."""
        return f'{self.name}'
 
 
