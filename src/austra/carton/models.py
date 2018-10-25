80bd49b1d3a105502a527aba3ff45e35a9d2e559
bakerdonkey <samkochanski@gmail.com>
2018-10-25 14:11:03 -0400

    init models

--- src/austra/carton/models.py
+++ src/austra/carton/models.py
@@ -10,6 +10,13 @@ class Class(models.Model) :
     def __str__(self) :
         return f"{self.code} {self.name}"
 
-    
+class Session(models.Model) :
+    """Model representing a session"""
+    cur_class = models.ForeignKey("Class", on_delete=models.SET_NULL, null=True)
+    instructor = models.ForeignKey("Instructor", on_delete=models.)
 
-# Create your models here.
+class Instructor(models.Model) :
+    """Model representing an instructor"""
+    name = models.CharField(max_length=50, help="Enter instructor name")
+    def __str__(self) :
+        return f"{self.name}"
