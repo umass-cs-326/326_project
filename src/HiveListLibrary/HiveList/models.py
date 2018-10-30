5364fa9d434f5c2ee509c4183cf6b3e29e0eac85
Collin Giguere <cdgiguere@umass.edu>
2018-10-30 18:04:54 +0000

    added import line to models.py

--- src/HiveListLibrary/HiveList/models.py
+++ src/HiveListLibrary/HiveList/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 import datetime
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
 now = datetime.datetime.now()
 
