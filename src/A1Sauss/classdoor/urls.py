fd378b28e41eb4e605e0189dc3a7937767e40bbd
Naflin <nathanblue00@gmail.com>
2018-11-03 18:54:24 -0400

    Added dynamic data for class page

--- src/A1Sauss/classdoor/urls.py
+++ src/A1Sauss/classdoor/urls.py
@@ -5,9 +5,10 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path("", views.index, name="index"),
     path("class/", RedirectView.as_view(url='/feed/',permanent=True)),
-    path("class/<int:pk>", views.classpage, name="class-detail"),
+    path("class/<int:id>", views.classpage, name="class-detail"),
     path("feed/", views.feed, name="feed"),
     path("login/", views.login, name="login"),
     path("profile/", views.profile, name="profile"),
-    path("review/", views.review, name="review"),
+    path("review/", RedirectView.as_view(url='/feed/',permanent=True)),
+    path("review/<int:pk>", views.review, name="review-detail")
 ]
--- src/A1Sauss/classdoor/views.py
+++ src/A1Sauss/classdoor/views.py
@@ -1,5 +1,4 @@
 import re
-
 from django.shortcuts import render
 from classdoor.models import Course, Teacher, Review, University, User, Subject
 
@@ -7,10 +6,47 @@ from classdoor.models import Course, Teacher, Review, University, User, Subject
 def index(request):
     return render(request, "index.html")
 
-def classpage(request, name):
-    data = Course.objects.all()
-    print(data)
-    return render(request, "class.html", {'data':data})
+def classpage(request, id):
+    # Get the individual course by id from url
+    course = Course.objects.get(pk=id)
+
+    courseData = {}
+    courseData["class"] = course
+    courseData["name"] = course.name
+    courseData["description"] = course.description
+    courseData["teacher"] = course.teacher
+    courseData["star_rating"] = course.starRating
+    courseData["reviews"] = course.reviews
+    courseData["average_grade"] = course.averageGrade
+    courseData["subject"] = course.subject
+    courseData["universityName"] = course.university_name
+
+    # Get all the reviews for the course
+    reviews = course.reviews.all()
+
+    reviewList = []
+
+    # Get the information about all the individual reviews in the list
+    for rdata in reviews:
+        reviewData = {}
+        reviewData["review"] = rdata
+        reviewData["title"] = rdata.title
+        reviewData["text"] = rdata.text
+        reviewData["star_rating"] = rdata.starRating
+        reviewData["grade_received"] = rdata.gradeReceived
+        reviewData["date"] = rdata.date
+        reviewData["tags"] = rdata.tags
+        reviewData["author"] = rdata.author
+
+        reviewList.append(reviewData)
+
+    # Add to course information and the list of reviews to the page context
+    context = {
+        "class": courseData,
+        "review_list": reviewList,
+    }
+
+    return render(request, "class.html", context=context)
 
 def feed(request):
     context = {}
