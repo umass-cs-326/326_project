dfa320f58ebae65f296bebbce3e3b5737483f1de
Naflin <nathanblue00@gmail.com>
2018-11-03 14:50:39 -0400

    classpage changes

--- src/A1Sauss/classdoor/urls.py
+++ src/A1Sauss/classdoor/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("class/", views.classpage, name="classpage"),
+    path("class/", RedirectView.as_view(url='/feed/',permanent=True)),
+    path("class/<str:name>/", views.classpage, name="classpage"),
     path("feed/", views.feed, name="feed"),
     path("login/", views.login, name="login"),
     path("profile/", views.profile, name="profile"),
--- src/A1Sauss/classdoor/views.py
+++ src/A1Sauss/classdoor/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from classdoor.models import Course, Teacher, Review, University, User, Subject
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
-def classpage(request):
-    return render(request, "class.html")
+def classpage(request, name):
+    data = Course.objects.all()
+    print(data)
+    return render(request, "class.html", {'data':data})
 
 def feed(request):
     return render(request, "feed.html")
