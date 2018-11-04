6827d898f11906e731452285431521ae4ca56b4e
crcarr@umass.edu <vagrant@vagrant.vm>
2018-11-04 19:17:20 +0000

    added logo and css

--- src/HiveListLibrary/HiveList/urls.py
+++ src/HiveListLibrary/HiveList/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . imort views
+from . import views
 
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("songs/", views.SongListView.as_view(), name="songs"),
-    path("song/<int:pk>", views.SongDetailView.as_view(), name="song-detail"),
+    #path("songs/", views.SongListView.as_view(), name="songs"),
+    #path("song/<int:pk>", views.SongDetailView.as_view(), name="song-detail"),
 ]
 
