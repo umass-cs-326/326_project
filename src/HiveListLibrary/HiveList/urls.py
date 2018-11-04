3b7b5a8bcb31f830d202e825ade4601ce5f82470
Collin Giguere <cdgiguere@umass.edu>
2018-11-04 18:53:57 +0000

    urls

--- src/HiveListLibrary/HiveList/urls.py
+++ src/HiveListLibrary/HiveList/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views
+from . imort views
 
 
 urlpatterns = [
     path("", views.index, name="index"),
-    
+    path("songs/", views.SongListView.as_view(), name="songs"),
+    path("song/<int:pk>", views.SongDetailView.as_view(), name="song-detail"),
 ]
 
