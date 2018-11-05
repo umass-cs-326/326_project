df3e2348f9b9c8b42ae63b48f7329bb9e0d0c2ee
bakerdonkey <samkochanski@gmail.com>
2018-11-05 16:31:21 +0000

    simple admin site

--- src/austra/carton/admin.py
+++ src/austra/carton/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from carton.models import Course, Instructor, Session
+
+
+class CourseAdmin(admin.ModelAdmin) :
+	list_display = ("code", "name", "rating", "display_prereqs")
+
+admin.site.register(Course, CourseAdmin)
+
+
+class InstructorAdmin(admin.ModelAdmin) :
+	list_display = ("name", "rating")
+
+admin.site.register(Instructor, InstructorAdmin)
+
+class SessionAdmin(admin.ModelAdmin) :
+	list_display = ("course", "instructor", "max_seats", "get_rating")
+
+admin.site.register(Session, SessionAdmin)
