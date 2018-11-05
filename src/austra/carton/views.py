cf1bd2d2bb239d80766a8cfa4ccb9711b140327b
partyrico <cjmoynihan@gmail.com>
2018-11-05 16:54:31 -0500

    Fixed up the calendar function in views.py  so that it passes in the Session objects (the classes). main.html was changed to support this.

--- src/austra/carton/views.py
+++ src/austra/carton/views.py
@@ -4,6 +4,7 @@ from django.views import generic
 from django.template import Context
 from django.template.loader import get_template
 from django.http import HttpResponse
+from .models import Session
 
 # Create your views here.
 #class ClassSessionView(generic.DetailView):
@@ -13,10 +14,16 @@ from django.http import HttpResponse
 
 def calendar(request):
     
-    template = get_template('main.html')
-    context = {
-    }
-    return HttpResponse(template.render(context, request))
+    # template = get_template('main.html')
+    # context = {
+    # }
+    sessions = [
+        [session.course.name, '{0}:00'.format(i+1), '{0}:00'.format(i+2), [1,3,5]]
+        for (i, session)
+        in enumerate(Session.objects.all())
+    ]
+    return render(request, 'main.html', {"sessions": sessions})
+    # return HttpResponse(template.render(context, request))
     
 def index(request):
     
