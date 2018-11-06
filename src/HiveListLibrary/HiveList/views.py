c3d53855db0a534b1de856ce47fc7615ffddb2ac
Omar Sanchez <osanchez@umass.edu>
2018-11-06 05:05:12 +0000

    changed url mappings to template files so all links are functional

--- src/HiveListLibrary/HiveList/views.py
+++ src/HiveListLibrary/HiveList/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 from HiveList.models import Playlist, Contributors, Artist, Song, Genre, SongInstance
 
+
 # Create your views here.
 def index(request):
-
     context = {
         "test": 5,
         "test2": 13,
@@ -15,82 +15,80 @@ def index(request):
 
 
 def currentPlaylist(request):
-
     playlist = Playlist.objects.order_by("?").first()
     all_songInstances = SongInstance.objects.filter(playlist_id__exact=playlist.playlist_id).values('song_id')
     all_songs = Song.objects.filter(song_id__in=all_songInstances)
     # Render the HTML tmeplate index.html with the data in the context variable
     context = {
-            "songs" : all_songs,
-            "current_playlist" : playlist
-            }
+        "songs": all_songs,
+        "current_playlist": playlist
+    }
     return render(request, "currentPlaylist.html", context=context)
 
-def Explore(request):
 
+def Explore(request):
     playlists = Playlist.objects.all()[:10]
     playlist_ids = Playlist.objects.all()[:10].values('playlist_id')
     all_songInstances = SongInstance.objects.filter(playlist_id__in=playlist_ids).values('song_id')
     songs = Song.objects.filter(song_id__in=all_songInstances)
     context = {
-            "popular_playlists" : playlists,
-            "popular_songs" : songs,
-            }
+        "popular_playlists": playlists,
+        "popular_songs": songs,
+    }
     # Render the HTML tmeplate index.html with the data in the context variable
     return render(request, "Explore.html", context=context)
 
-def Home(request):
 
-    #IP_playlists = pass
-    top_100_songs = Playlist.objects.all()[:100]              
-    public_playlists_var = Playlist.objects.filter(playlist_is_private__exact=False)
-    #profile_information = pass
-    #playlist_export = pass
+def Home(request):
+    # IP_playlists = pass
+    top_10_playlists = Playlist.objects.all()[0:10]
+    public_playlists_var = Playlist.objects.filter(playlist_is_private__exact=False)[0:5]  # TODO: make pages
+    # profile_information = pass
+    # playlist_export = pass
 
     context = {
-    #"IP_playlists": IP_playlists
-    "top_100" : top_100_songs,
-    "public_playlists" : public_playlists_var,
-    #"profile_information": profile_information
-    #"playlist_export": playlist_export
+        # "IP_playlists": IP_playlists
+        "top_10": top_10_playlists,
+        "public_playlists": public_playlists_var,
+        # "profile_information": profile_information
+        # "playlist_export": playlist_export
     }
 
     # Render the HTML tmeplate index.html with the data in the context variable
     return render(request, "Home.html", context=context)
+
 
 def myLists(request):
     IP_playlists = Playlist.objects.all()[:10]
     My_playlists = Playlist.objects.all()[11:21]
 
     context = {
-            "My_IP_Playlists" : IP_playlists,
-            "My_Finished_Playlists" : My_playlists,
-            }
+        "My_IP_Playlists": IP_playlists,
+        "My_Finished_Playlists": My_playlists,
+    }
     # Render the HTML tmeplate index.html with the data in the context variable
     return render(request, "myLists.html", context=context)
+
 
 def playlistSettings(request):
     playlist = Playlist.objects.order_by("?").first()
     all_songInstances = SongInstance.objects.filter(playlist_id__exact=playlist.playlist_id).count()
 
     context = {
-            "song_count" : all_songInstances,
-            "playlist" : playlist,
-            }
-    
+        "song_count": all_songInstances,
+        "playlist": playlist,
+    }
+
     # Render the HTML tmeplate index.html with the data in the context variable
     return render(request, "playlistSettings.html", context=context)
 
-def profile(request):
 
+def profile(request):
     playlist = Playlist.objects.order_by("?").first()
     all_songInstances = SongInstance.objects.filter(playlist_id__exact=playlist.playlist_id).values('song_id')
     all_songs = Song.objects.filter(song_id__in=all_songInstances)
     context = {
-            "songs" : all_songs,
-            }
+        "songs": all_songs,
+    }
     # Render the HTML tmeplate index.html with the data in the context variable
     return render(request, "profile.html", context=context)
-    
-
-
