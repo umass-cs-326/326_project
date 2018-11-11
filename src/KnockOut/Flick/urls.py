f57d2acda6a809993a662d653923a31a40c27e87
frickshao <43217796+frickshao@users.noreply.github.com>
2018-11-11 17:44:13 -0500

    Update urls.py

--- src/KnockOut/Flick/urls.py
+++ src/KnockOut/Flick/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index/", views.index, name="index"), #movie list page
-    path("movie/<str:movie>/", views.movie, name="movie"),
-    path("user/<str:user>", views.user, name="user"),
-    path("matchbox/", views.matchbox, name="matchbox"),
+    path('index/', views.index, name='index'), #movie list page
+    path('movie/<str:movie>/', views.movie, name='movie'),
+    path('user/<str:user>', views.user, name='user'),
+    path('matchbox/', views.matchbox, name='matchbox'),
 ]
