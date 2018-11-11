e7419e19e26040e8fc0e4ef2b7787a0c9e6bde1d
frickshao <43217796+frickshao@users.noreply.github.com>
2018-11-11 17:51:32 -0500

    Update views.py

--- src/KnockOut/Flick/views.py
+++ src/KnockOut/Flick/views.py
@@ -44,7 +44,6 @@ def user(request, userid=None):
     context={
             "username" : user_object.username,
             "bio" : user_object.bio,
-            "pic" : user_object.pic,
             }
     return render(
             request,
