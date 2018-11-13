905ed96e88b9c0344549aa37d712fe1320209a46
jhui04 <jhui@umass.edu>
2018-11-13 13:48:36 -0500

    added path to login.html and moved into Catch directory

--- CS326_project/Catch/urls.py
+++ CS326_project/Catch/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls import include
 
 
 urlpatterns = [
@@ -19,3 +20,5 @@ urlpatterns = [
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [path(r'accounts/', include('django.contrib.auth.urls')),
+]
