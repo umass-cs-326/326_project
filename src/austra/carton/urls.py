3e03bb14b04d373b487bf615b2c3fb473ac00614
vagrant <hoyogo1@gmail.com>
2018-12-11 19:03:11 +0000

    tree

--- src/austra/carton/urls.py
+++ src/austra/carton/urls.py
@@ -5,7 +5,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.index, name = "index"),
-    path('tree/', TemplateView.as_view(template_name="tree.html"), name = "tree"),
     # path('accounts/profile/', TemplateView.as_view(template_name="accounts/profile.html"), name = "profile"),
     path('accounts/profile/', views.profile_page, name = "profile"),
     path('calendar/', views.calendar, name = "calendar"),
