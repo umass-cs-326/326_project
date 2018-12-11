48467082db8f24b2e4f9dab0ea47a14ce86f41ac
Erica Zheng <weizheng@umass.edu>
2018-12-11 17:26:36 -0500

    cleaned urls

--- CS326_project/Catch/urls.py
+++ CS326_project/Catch/urls.py
@@ -8,26 +8,24 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    # path('', views.home, name = "homePage"),
+    
     path('homePage', views.home, name = "homePage"),
     path('eventsPage', views.events, name = "eventsPage"),
-
     path('mapPage', views.map, name = "mapPage"),
     path('profilePage', views.UserViewProfileView.as_view(), name='profilePage'),
-
-    # path('aboutPage', views.about, name = "aboutPage"),
     path('petPage', views.UserViewPets.as_view(), name = "petPage"),
-
     path('addEvent', views.EventCreate.as_view(success_url='homePage'), name='Event-add'),
     path('addPet', views.PetCreate.as_view(), name='Pet-add'),
-    # path('add/$', views.EventCreate.as_view(success_url='homePage.html'), name='Event-add'),
-    # path('navbar', views.navbar, name = "navbar"),
-
     path('accounts/signup', views.UserSignUpView.as_view(), name='user-sign-up'),
     path('account/edit', views.UserEditProfileView.as_view(), name='user-edit-profile'),
     path('account/view', views.UserViewProfileView.as_view(), name='user-view-profile'),
     path('account/login', views.UserViewProfileView.as_view(), name='user-view-profile'),
+    
     #path('profile/edit', views.edit_profile, name='edit_profile')
+    # path('', views.home, name = "homePage"),
+    # path('aboutPage', views.about, name = "aboutPage"),
+    # path('add/$', views.EventCreate.as_view(success_url='homePage.html'), name='Event-add'),
+    # path('navbar', views.navbar, name = "navbar"),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
